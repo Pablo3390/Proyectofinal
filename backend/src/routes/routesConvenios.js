9cdbd25281c1064d47481b7e12278f3f73764f19
Pablo3390 <pablosaucedoa2@gmail.com>
2023-08-27 00:28:51 -0300

    Nueva base de datos

---
+++ backend/src/routes/routesConvenios.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express()
+const mysqlConect = require('../database/database');
+const bodyParser = require('body-parser');
+const bcrypt= require('bcrypt')
+const jwt= require('jsonwebtoken')
+
+
+
+module.exports= router; 
