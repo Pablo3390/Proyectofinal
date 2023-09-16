493d37329597794b5f3049184e3809f37321cf95
Pablo3390 <pablosaucedoa2@gmail.com>
2023-09-15 23:07:18 -0300

    Arreglo del metodo put en convenios

--- backend/src/routes/routesConvenio.js
+++ backend/src/routes/routesConvenio.js
@@ -86,60 +86,66 @@ router.get('/convenio/:id_convenio', (req , res)=>{
 //Metodo UPDATE
 //Borrado logico por id
 
-router.put('/convenios/:id_convenio', bodyParser.json(), (req , res)=>{
-  const { id_convenio } = req.params
-  const { nombre, utilidad, objeto, fecha_inicio, fecha_fin, clausula_peas, id_resolucion, id_organismo, id_tipo_convenio  } = req.body
-  console.log("esto es el body",req.body)
-  if(!nombre){
-      res.json({
-          status:false,
-          mensaje: "El nombre del convenio es un campo obligatorio"
-      })
-  }
-  if(!id_resolucion){
-      res.json({
-          status:false,
-          mensaje: "La resolucion del convenio es un campo obligatorio"
-      })
-  }
-  if(!id_tipo_convenio){
-      res.json({
-          status:false,
-          mensaje: "El tipo de convenio es un campo obligatorio"
-      })
-  }
-  if(!id_organismo){
-      res.json({
-          status:false,
-          mensaje: "El organismo del convenio es un campo obligatorio"
-      })
-  }
-  mysqlConect.query('SELECT * FROM convenios WHERE id_convenio=?', [id_convenio], (error, registros)=>{
-      if(error){
-          console.log('Error en la base de datos', error)
-      }else{
+router.put('/convenio/:id_convenio',bodyParser.json(), (req,res)=>{ 
+    const {id_convenio}= req.params
+    const {nombre, utilidad, objeto, fecha_inicio, fecha_fin, clausula_peas, id_resolucion, id_organismo, id_tipo_convenio }=req.body
 
-          if(registros.length>0){
-              mysqlConect.query('UPDATE convenio SET nombre=?, utilidad=?, objeto=?, fecha_inicio=?, fecha_fin=?, clausula_peas=?, id_resolucion=?, idorganismo=?, id_tipo_convenio=?  WHERE id_convenio = ?', [nombre, utilidad, objeto, fecha_inicio, fecha_fin, clausula_peas, id_resolucion, id_organismo, id_tipo_convenio, id_convenio ], (error, registros)=>{
-                  if(error){
-                      console.log('Error en la base de datos', error)
-                  }else{
-                      res.json({
-                          status:true,
-                          mensaje:"El registro " +id_convenio+ " se edito correctamente" 
-                      })
-                  }
-              })
-          }else{
-              res.json({
-                  status:false,
-                  mensaje:"El ID del convenio no existe" 
-              })
-          }
-          
-      }
-  })  
-})
+    if(!nombre){
+        res.json({
+            status: false,
+            mensaje: "el nombre del convenio es un dato obligatorio"
+        })
+    }
+    
+    if(!id_organismo){
+        res.json({
+            status: false,
+            mensaje: "el id_organismo es un dato obligatorio"
+        })
+    }
+    if(!id_tipo_convenio){
+        res.json({
+            status: false,
+            mensaje: "el id_tipo_convenio es un dato obligatorio"
+        })
+    }
+
+    if(!id_resolucion){
+        res.json({
+            status: false,
+            mensaje: "el id_resolucion es un dato obligatorio"
+        })
+    }
+
+    mysqlConect.query('SELECT * FROM convenio WHERE id_convenio=?;', [id_convenio], (error, registro) =>{
+        if(error){
+            console.log("Error en la base de datos", error)
+        
+        }else{
+            if(registro.length>0){
+                mysqlConect.query('UPDATE convenio SET nombre=?, utilidad=?, objeto=?, fecha_inicio=?, fecha_fin=?, clausula_peas=?, id_resolucion=?, id_organismo=?, id_tipo_convenio=? WHERE id_convenio = ?', [nombre, utilidad, objeto, fecha_inicio, fecha_fin, clausula_peas, id_resolucion, id_organismo, id_tipo_convenio, id_convenio], (error, registro) =>{
+                    
+                    if(error){
+                                console.log("Error en la base de datos", error)
+                    }else{ 
+                        res.json({
+                            status: true,
+                            mensaje: 'El registro '+id_convenio+ ' se editó correctamente'
+                        })
+                                
+                    }
+                 })
+
+            }else{
+            res.json({
+                status:false,
+                mensaje: "El id del convenio no existe"
+            })
+        }   
+        }
+    })
+    })
+
 
 // Metodo DELETE
 //Borrado logico por id
