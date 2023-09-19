50730649f8f5ddfd5becb1ab719931071e849bde
Pablo3390 <pablosaucedoa2@gmail.com>
2023-09-19 15:14:06 -0300

    Como declarar rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 
 
 import './App.css'
-import { Menu } from './Menu'
+
 import { Home } from './Home'
 import { Route, Routes } from 'react-router-dom'
+import { Login } from './Login'
+import { Principal } from './Principal'
 
 
 function App() {
@@ -14,9 +16,11 @@ function App() {
   return (
     <>
         
-      <Menu/>
+      
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/Login' element={<Login/>}></Route>
+        <Route path='/Principal' element={<Principal/>}></Route>
       </Routes>
 
     </>
---
+++ frontend/src/Principal.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Menu } from './Menu'
+export function Principal(){
+    return(
+        <>
+        <Menu/>
+        Esta es mi pantalla principal
+        </>
+    )
+}
