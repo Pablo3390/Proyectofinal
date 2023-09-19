a9f678a57c432dc71681885332cc6426a3e14ed8
Pablo3390 <pablosaucedoa2@gmail.com>
2023-09-19 13:34:18 -0300

    Modificacion en el fronnt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,31 @@
-
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { Home } from './Home'
-import { Route, Routes } from 'react-router-dom'
-import { Login } from './Login'
-import { Principal } from './Principal'
-import { Registro } from './Registro'
+import { Menu } from './Menu'
+
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <>    
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/principal' element={<Principal/>}></Route>
-      <Route path='/registro' element={<Registro/>}></Route>
-
-
-    </Routes>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Esta es la pantalla principal</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        
+      </div>   
+      <Menu/>
 
     </>
   )
